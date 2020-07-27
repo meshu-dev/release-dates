2f4d58480165e7e3993cf76434f5a5e33e883b25
meshu-dev <admin@requiredev.com>
2020-07-27 15:40:29 +0100

    Removed welcome route and page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<b-navbar toggleable="lg" type="dark" variant="danger">
+    <b-container>
+        <b-navbar-brand href="#">Release Dates</b-navbar-brand>
+        <b-navbar-nav class="ml-auto">
+            <template v-if="authenticated">
+                <b-nav-item href="#">Add Release</b-nav-item>
+                <b-nav-item href="#" @click="logout">Logout</b-nav-item>
+            </template>
+        </b-navbar-nav>
+    </b-container>
+</b-navbar>
+<b-container id="app-content">
+    <div class="flex-center position-ref full-height">
+        <template v-if="authenticated">
+            <release-component></release-component>
+        </template>
+        <template v-else>
+            <login-component @loggedin="loggedIn"></login-component>
+        </template>
     </div>
-</div>
+</b-container>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,27 +19,7 @@
     </head>
     <body>
         <div id="app">
-            <b-navbar toggleable="lg" type="dark" variant="danger">
-                <b-container>
-                    <b-navbar-brand href="#">Release Dates</b-navbar-brand>
-                    <b-navbar-nav class="ml-auto">
-                        <template v-if="authenticated">
-                            <b-nav-item href="#">Add Release</b-nav-item>
-                            <b-nav-item href="#" @click="logout">Logout</b-nav-item>
-                        </template>
-                    </b-navbar-nav>
-                </b-container>
-            </b-navbar>
-            <b-container id="app-content">
-                <div class="flex-center position-ref full-height">
-                    <template v-if="authenticated">
-                        <release-component></release-component>
-                    </template>
-                    <template v-else>
-                        <login-component @loggedin="loggedIn"></login-component>
-                    </template>
-                </div>
-            </b-container>
+            @yield('content')
         </div>
     </body>
 </html>
